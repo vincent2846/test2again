ba5a844a9ea107b933c218c5bcde111613a11fa9
Vincent Novianto <vincentnovianto16@gmail.com>
2023-08-18 13:17:17 +0700

    Form dan Validation test 1

--- src/Materi/FormAndValidation/index.jsx
+++ src/Materi/FormAndValidation/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import FormElement from "./Pembahasan/FormElement";
-import Validation from "./Pembahasan/Validation";
+
 export default class FormAndValidation extends React.Component{
 
 
